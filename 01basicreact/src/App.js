d546b48673f753195abbb6e208a971018d0e50b4
muskan101v <varshney.muskan1998@gmail.com>
2023-11-22 23:55:24 +0530

    create project with npx and vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>chai or react</h1>;
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+
+
+  return (
+  <h1>chai aur vite react</h1>
+  )
+}
+
+export default App
